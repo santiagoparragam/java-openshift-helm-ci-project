19343776d4fc000bef777b9537975f6d8e868868
santiagoParraga <santiago.parraga@modinter.com.ec>
2025-11-11 21:36:14 -0500

    init 11112025

--- microservice-b/src/main/java/com/example/b/RecordRepository.java
+++ microservice-b/src/main/java/com/example/b/RecordRepository.java
@@ -3,7 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // NOTE: Replace 'com.yourcompany.model' with the actual package of your RecordEntity
-import com.yourcompany.model.RecordEntity; 
+
 
 @Repository
 public interface RecordRepository extends JpaRepository<Person, Long> {
