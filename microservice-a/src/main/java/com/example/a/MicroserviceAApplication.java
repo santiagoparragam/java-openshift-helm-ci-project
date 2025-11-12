4e66d6f41eedded314a05120443fbe4800312918
santiagoParraga <santiago.parraga@modinter.com.ec>
2025-11-11 21:18:54 -0500

    init 11112025

--- microservice-a/src/main/java/com/example/a/MicroserviceAApplication.java
+++ microservice-a/src/main/java/com/example/a/MicroserviceAApplication.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Map;
+import java.lang.System;
 
 
 
